d56fc91998eaa448a91c06fa702f5dc1bd4514be
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-18 22:39:56 +0300

    upd

---
+++ src/serialcmd/StreamBinarySerializer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <Stream.h>
+
+
+namespace serialcmd {
+
+    /// Вспомогательный класс для чтения структур и примитивных типов из потока
+    class StreamBinarySerializer {
+    private:
+
+        Stream &stream;
+
+    public:
+        explicit StreamBinarySerializer(Stream &stream) :
+            stream(stream) {}
+
+        /// Побайтово считать тип
+        template<class T> void read(T &destination) {
+            this->stream.readBytes((uint8_t *) &destination, sizeof(T));
+        }
+
+        /// Побайтово записать тип
+        template<class T> void write(T &source) {
+            this->stream.write((const uint8_t *) &source, sizeof(T));
+        }
+    };
+}
