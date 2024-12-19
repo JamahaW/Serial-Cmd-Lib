1d5a9c10d48a613e3f22a6285bdc6135bc1288c6
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-20 00:42:28 +0300

    upd

--- src/serialcmd/StreamSerializer.hpp
+++ src/serialcmd/StreamSerializer.hpp
@@ -6,23 +6,23 @@
 namespace serialcmd {
 
     /// Вспомогательный класс для чтения структур и примитивных типов из потока
-    class StreamBinarySerializer {
+    class StreamSerializer {
     private:
 
         Stream &stream;
 
     public:
-        explicit StreamBinarySerializer(Stream &stream) :
+        explicit StreamSerializer(Stream &stream) :
             stream(stream) {}
 
         /// Побайтово считать тип
         template<class T> void read(T &destination) {
-            this->stream.readBytes((uint8_t *) &destination, sizeof(T));
+            this->stream.readBytes(reinterpret_cast<uint8_t *>(&destination), sizeof(T));
         }
 
         /// Побайтово записать тип
         template<class T> void write(T &&source) {
-            this->stream.write((const uint8_t *) &source, sizeof(T));
+            this->stream.write(reinterpret_cast<const uint8_t *>(&source), sizeof(T));
         }
     };
 }
